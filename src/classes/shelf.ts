f9a1b4d9733a314afcb1d7d22302f8e7cdcb2f44
Maksym Makarov <Maksym_Makarov@epam.com>
2019-05-19 23:26:04 +0200

    add commit to generic function

--- src/classes/shelf.ts
+++ src/classes/shelf.ts
@@ -1,21 +1,24 @@
 import { ShelfItem } from "../interfaces";
 
+// we can use prop declaration directely
+// export default class Shelf <T extends {title: string}>
+// or use interface
 export default class Shelf<T extends ShelfItem> {
-  private _items: Array<T> = new Array<T>();
+    private _items: Array<T> = new Array<T>();
 
-  add(item: T): void {
-    this._items.push(item);
-  }
+    add(item: T): void {
+        this._items.push(item);
+    }
 
-  getFirst(): T {
-    return this._items[0];
-  }
+    getFirst(): T {
+        return this._items[0];
+    }
 
-  find(title: string): T {
-    return this._items.find(item => item.title === title);
-  }
+    find(title: string): T {
+        return this._items.find(item => item.title === title);
+    }
 
-  printTitles(): void {
-    this._items.forEach(item => console.log(item.title));
-  }
+    printTitles(): void {
+        this._items.forEach(item => console.log(item.title));
+    }
 }
